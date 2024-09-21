3bb07102bdcf68b2073d9d8708341a7be3699d1b
David Peñalver <david13penalver@gmail.com>
2024-09-21 17:12:51 +0200

    WDCE - First Evaluation: Calendar finished

--- Web Development in Client Environment/First Evaluation/calendario-mes-actual/calendario.js
+++ Web Development in Client Environment/First Evaluation/calendario-mes-actual/calendario.js
@@ -1,39 +1,39 @@
-let todayDate = new Date();
-let currentYear = todayDate.getFullYear();
-let currentMonth = todayDate.getMonth();
-let currentDay = todayDate.getDate();
-let currentDayOfWeek = todayDate.getDay();
-
 let monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
     "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
     ];
+let daysMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-let daysOfWeek = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+let todayDate = new Date();
+let currentYear = todayDate.getFullYear();
+let currentMonth = todayDate.getMonth();
+let daysCurrentMonth = daysMonth[currentMonth];
+let currentDay = todayDate.getDate();
+currentDay = (currentDay == 0) ? 7 : currentDay;
 
 document.getElementById("title").innerHTML = `${monthNames[currentMonth]} de ${currentYear}`;
 
 document.getElementById("table-head").innerHTML += `
     <tr>
-        <td>${daysOfWeek[1]}</td>
-        <td>${daysOfWeek[2]}</td>
-        <td>${daysOfWeek[3]}</td>
-        <td>${daysOfWeek[4]}</td>
-        <td>${daysOfWeek[5]}</td>
-        <td>${daysOfWeek[6]}</td>
-        <td>${daysOfWeek[0]}</td>
-    </tr>`
+        <td>Lunes</td>
+        <td>Martes</td>
+        <td>Miércoles</td>
+        <td>Jueves</td>
+        <td>Viernes</td>
+        <td>Sábado</td>
+        <td>Domingo</td>
+    </tr>`;
 
-let firstDayOfMonthOnWeek = new Date(currentYear, currentMonth, 1);
-let firstDayOfMonthOnWeekArrayed = daysOfWeek[firstDayOfMonthOnWeek.getDay()];
+let inicio = 1 - (currentDay - 1)
 
-alert(firstDayOfMonthOnWeekArrayed);
+let cal = `<table border=1>`;
+n = 0
+    for (i = inicio; i <= daysCurrentMonth; i++) {
+        if (n % 7 == 0) cal += "<tr>"
+        let texto = (i <= 0) ? " " : i
+        cal += "<td>" + texto
+        n++
+    }
 
-if (firstDayOfMonthOnWeek.getDay == 0) {
-    firstDayOfMonthOnWeek = 7;
-}
+cal += "</table>"
 
-alert(firstDayOfMonthOnWeek)
-
-for (let i = 0; i == firstDayOfMonthOnWeek; i++) {
-    <td>document.getElementById("table-body").innerHTML += `0`</td>
-}
+document.getElementById("table-head").innerHTML += cal;
