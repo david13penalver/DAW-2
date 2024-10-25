ad6e27825f44cb4d0b21efd6d622cf22fb323b31
David Peñalver <david13penalver@gmail.com>
2024-10-25 10:24:35 +0200

    BAE - First Evaluation: Phase 1 and Phase 2

--- Web Development in Server Environment/First Evaluation/Bookstore_structure/bookstore/src/main/java/com/fpmislata/bookstore/persistence/admin/common/CustomRowMapper/BookRowMapperAdmin.java
+++ Web Development in Server Environment/First Evaluation/Bookstore_structure/bookstore/src/main/java/com/fpmislata/bookstore/persistence/admin/common/CustomRowMapper/BookRowMapperAdmin.java
@@ -1,9 +1,17 @@
 package com.fpmislata.bookstore.persistence.admin.common.CustomRowMapper;
 
 import com.fpmislata.bookstore.domain.admin.model.Book;
+import org.springframework.jdbc.core.RowMapper;
 
-public class BookRowMapperAdmin {
-    Book book = new Book();
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookRowMapperAdmin implements RowMapper<Book> {
+
+    @Override
+    public Book mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Book book = new Book();
         book.setId(resultSet.getLong("id"));
         book.setIsbn(resultSet.getString("isbn"));
         book.setTitleEs(resultSet.getString("title_es"));
@@ -13,12 +21,24 @@ public class BookRowMapperAdmin {
         book.setPrice(new BigDecimal(resultSet.getString("books.price")));
         book.setDiscount(resultSet.getFloat("books.discount"));
         book.setCover(resultSet.getString("books.cover"));
-        if(this.existsColumn(resultSet, "publishers.id")) {
-        book.setPublisher(publisherRowMapper.mapRow(resultSet, rowNum));
-    }
-        if(this.existsColumn(resultSet, "categories.id")) {
-        book.setCategory(categoryRowMapper.mapRow(resultSet, rowNum));
-    }
+
+        // Assuming existsColumn is a method in this class or a utility class
+//        if (existsColumn(resultSet, "publishers.id")) {
+//            book.setPublisher(publisherRowMapper.mapRow(resultSet, rowNum));
+//        }
+//        if (existsColumn(resultSet, "categories.id")) {
+//            book.setCategory(categoryRowMapper.mapRow(resultSet, rowNum));
+//        }
         return book;
-}
+    }
+
+    // Dummy implementation of existsColumn method
+    private boolean existsColumn(ResultSet rs, String columnName) {
+        try {
+            rs.findColumn(columnName);
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
 }
