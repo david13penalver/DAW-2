2419cb9524e16eb86bb55d1b06b1103fe091ac09
David Peñalver <david13penalver@gmail.com>
2024-12-13 11:40:41 +0100

    WAD - Second Evaluation: Build.sh with Sass

--- Web Apps Deployment/Second Evaluation/Class_exercices/2024-11-15_spring/demo/scripts/sass.sh
+++ Web Apps Deployment/Second Evaluation/Class_exercices/2024-11-15_spring/demo/scripts/sass.sh
@@ -1,1 +1,3 @@
-sass ./src/main/resources/static/scss/main.scss ./src/main/resources/static/css/main.css
+sass ./target/classes/static/scss/main.scss ./src/main/resources/static/css/main.css
+sass --no-source-map --style=compressed ./target/classes/static/scss/main.scss ./target/classes/static/css/main.css
+rm -r ./target/classes/static/scss/*
