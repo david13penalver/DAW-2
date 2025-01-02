9bcca521810f3361d5d8e2f0d097451079ad5ebd
David Peñalver <david13penalver@gmail.com>
2025-01-02 17:13:14 +0100

    WDCE - Second Evaluation: Class exercises prepared

--- Web Apps Deployment/Second Evaluation/Class_exercices/External_server_deployment/FacturasDespliegue/scripts/deploy.sh
+++ Web Apps Deployment/Second Evaluation/Class_exercices/External_server_deployment/FacturasDespliegue/scripts/deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-pwd
-
-source ./entornos/develop.env
-
-echo $USER_NAME
+source $1
 
 cd ..
 
@@ -14,24 +10,30 @@ git clone https://github.com/shengye472/Facturas.git
 
 cd Facturas
 
-git switch develop
+git switch $BRANCH
 
 mvn clean install
 
 scp target/*.jar ${USER_NAME}@${IP_SERVER}:/home/${USER_NAME}/davidpn_deploy
 
-ssh ${USER_NAME}@$IP_SERVER "cd /home/${USER_NAME}/davidpn_deploy"
+# ssh ${USER_NAME}@$IP_SERVER "export JAVA_HOME=/home/${USER_NAME}/davidpn_deploy/jre21 && $JAVA_HOME/bin/java -D server.port=${APP_PORT} -jar *.jar"
 
-echo "Antes java -jar"
+echo "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
 
-# ssh ${USER_NAME}@$IP_SERVER "export JAVA_HOME=$(pwd)/jdk-21.0.5+11 && $JAVA_HOME/bin/java -D server.port=${APP_PORT} -jar *.jar"
+# ssh ${USER_NAME}@$IP_SERVER "export JAVA_HOME=/home/alumno/davidpn_deploy && echo aaa'$JAVA_HOME'bbbbb && echo 'hola mundo'"
 
-ssh ${USER_NAME}@$IP_SERVER "jdk-21.0.5+11/bin/java -D server.port=${APP_PORT} -jar *.jar"
+# ssh ${USER_NAME}@$IP_SERVER "/home/${USER_NAME}/davidpn_deploy/jre21/bin/java -jar /home/${USER_NAME}/davidpn_deploy/mi-proyecto-0.0.1-SNAPSHOT.jar" &
 
-echo "Después java -jar"
+cd ../FacturasDespliegue/scripts
+
+pwd
+
+scp server.sh $USER_NAME@$IP_SERVER:/home/alumno/davidpn_deploy
+
+ssh ${USER_NAME}@$IP_SERVER "./server.sh"
 
 sleep 1
-echo "Ejecutando desplegamiento"
+echo "Ejecutando despliegue"
 echo "."
 sleep 1
 echo "Deploy realizado."
---
+++ Web Apps Deployment/Second Evaluation/Class_exercices/External_server_deployment/FacturasDespliegue/scripts/server.sh
@@ -0,0 +1,3 @@
+cd /home/alumno/davidpn_deploy
+export JAVA_HOME=/home/alumno/davidpn_deploy/jre21
+$JAVA_HOME/bin/java -jar *.jar
