9acf64a09e68dd58887560235e558c169194569a
David Peñalver <david13penalver@gmail.com>
2025-01-17 19:29:33 +0100

    WDSE - Second Evaluation: JPA practice- Validation with Jakarta in Author class

--- Web Development in Server Environment/Second Evaluation/bookstore_jpa/src/main/java/com/fpmislata/bookstore/domain/model/Author.java
+++ Web Development in Server Environment/Second Evaluation/bookstore_jpa/src/main/java/com/fpmislata/bookstore/domain/model/Author.java
@@ -1,6 +1,10 @@
 package com.fpmislata.bookstore.domain.model;
 
 import com.fpmislata.bookstore.common.locale.LanguageUtils;
+import com.fpmislata.bookstore.common.validation.ValidYear;
+import jakarta.annotation.Nullable;
+import jakarta.validation.ValidationException;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +15,15 @@ import lombok.NoArgsConstructor;
 public class Author {
 
     private Long id;
+    @Nullable
     private String name;
     private String nationality;
     private String biographyEs;
     private String biographyEn;
-    private int birthYear;
-    private int deathYear;
+    @Min(value = -1880, message = "El año de nacimiento debe ser mayor o igual a 1880")
+    private Integer birthYear;
+    @ValidYear
+    private Integer deathYear;
 
     public String getBiography(){
         String language = LanguageUtils.getCurrentLanguage();
@@ -24,5 +31,19 @@ public class Author {
             return biographyEn;
         }
         return biographyEs;
+    }
+
+    public void setBirthYear(int birthYear) {
+        if (this.birthYear != null && this.deathYear!= null && this.birthYear > this.deathYear) {
+            throw new ValidationException("El año de nacimiento no puede ser mayor que el año de defunción");
+        }
+        this.birthYear = birthYear;
+    }
+
+    public void setDeathYear(int birthYear) {
+        if (this.birthYear != null && this.deathYear!= null && this.birthYear > this.deathYear) {
+            throw new ValidationException("El año de nacimiento no puede ser mayor que el año de defunción");
+        }
+        this.birthYear = birthYear;
     }
 }
