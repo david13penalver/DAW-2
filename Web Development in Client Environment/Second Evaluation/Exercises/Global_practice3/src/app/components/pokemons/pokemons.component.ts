3812f77b9d15f2c6406757c375a1ec0d61b7c68a
David Peñalver <david13penalver@gmail.com>
2025-02-07 09:30:56 +0100

    WDCE - Second Evaluation: Global exercise- Added Post method

--- Web Development in Client Environment/Second Evaluation/Exercises/Global_practice3/src/app/components/pokemons/pokemons.component.ts
+++ Web Development in Client Environment/Second Evaluation/Exercises/Global_practice3/src/app/components/pokemons/pokemons.component.ts
@@ -12,6 +12,8 @@ import {PokemonComponent} from '../pokemon/pokemon.component';
 export class PokemonsComponent {
   pokemons!:Pokemons[];
 
+  newPokemon!:Pokemons;
+
   constructor(private miServicio:PokemonServiceService) {
   }
 
@@ -27,7 +29,25 @@ export class PokemonsComponent {
   }
 
   nuevo() {
+    this.newPokemon = {
+      id: 0,
+      name: "",
+      type: "",
+      description: "",
+      height: "",
+      weight: ""
+    }
+    this.newPokemon.id = Number(prompt("Introduce el id del pokemon")) || this.newPokemon.id;
+    this.newPokemon.name = prompt("Introduce el nombre del pokemon") || this.newPokemon.name;
+    this.newPokemon.type = prompt("Introduce el tipo del pokemon") || this.newPokemon.type;
+    this.newPokemon.description = prompt("Introduce la descripción del pokemon") || this.newPokemon.description;
+    this.newPokemon.height = prompt("Introduce la altura del pokemon") || this.newPokemon.height;
+    this.newPokemon.weight = prompt("Introduce el peso del pokemon") || this.newPokemon.weight;
 
+    this.miServicio.post(this.newPokemon).subscribe({
+      next: () => this.cargar(),
+      error: error => console.log(error.status)
+    })
   }
 
   delete(id:number) {
