209282cd7d2c01609e31b4ea50d71380f65b2925
David Peñalver <david13penalver@gmail.com>
2025-04-13 22:12:03 +0200

    TFG: ExerciseController-findById fixed

--- TFG/backend/bs24/src/main/java/com/bs24/demo/controller/user/webmodel/ExerciseDetail.java
+++ TFG/backend/bs24/src/main/java/com/bs24/demo/controller/user/webmodel/ExerciseDetail.java
@@ -3,7 +3,10 @@ package com.bs24.demo.controller.user.webmodel;
 public record ExerciseDetail(
         //String id,
         String name,
+        String description,
         String imageURL,
+        String videoURL,
+        Boolean isGlobal,
         ExerciseTypeCollection exerciseType,
         ExerciseSubtypeCollection exerciseSubtype
 ) {
