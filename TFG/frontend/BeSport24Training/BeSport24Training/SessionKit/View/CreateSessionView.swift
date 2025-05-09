6358e6d0b30e8ee87f8cfdfb3a28281a0c1cea43
David Peñalver <david13penalver@gmail.com>
2025-05-09 19:57:23 +0200

    TFG: Documentation - Scaffolding, Launche Screen, Icon and API

---
+++ TFG/frontend/BeSport24Training/BeSport24Training/SessionKit/View/CreateSessionView.swift
@@ -0,0 +1,7 @@
+//
+//  CreateTrainingView.swift
+//  BeSport24Training
+//
+//  Created by David Peñalver Navarro on 9/5/25.
+//
+
---
+++ TFG/frontend/BeSport24Training/BeSport24Training/TrainingKit/View/EditTrainingView.swift
@@ -0,0 +1,7 @@
+//
+//  EditTrainingView.swift
+//  BeSport24Training
+//
+//  Created by David Peñalver Navarro on 9/5/25.
+//
+
